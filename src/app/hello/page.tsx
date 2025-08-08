bf7dd32daf7d3bb049aea8355de27ea2e357b689
Diaz  Luthfi <diazluthfi07@gmail.com>
2025-08-08 15:01:00 +0700

    farhan cantik

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -3,7 +3,7 @@
 export default function HelloPage() {
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Selamat pagi!</h1>
+      <h1>Selamat pagi sayang!</h1>
     </div>
   );
 }
