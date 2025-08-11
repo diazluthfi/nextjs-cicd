26bbbbc90ca7fadda2e1710b34885bee07d9b056
Diaz  Luthfi <diazluthfi07@gmail.com>
2025-08-11 15:58:04 +0700

    up adhit

--- k6.js
+++ k6.js
@@ -6,7 +6,7 @@ const BASE_URL =
 
 export let options = {
   stages: [
-    { duration: "1m", target: 1000 }, // 10 users selama 1 menit
+    { duration: "1m", target: 10 }, // 10 users selama 1 menit
   ],
 };
 
