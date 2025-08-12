a4be37b9e65db23b62d2be52b62b5e3e3f436760
Diaz  Luthfi <diazluthfi07@gmail.com>
2025-08-12 10:55:08 +0700

    parhan

--- k6.js
+++ k6.js
@@ -6,7 +6,7 @@ const BASE_URL =
 
 export let options = {
   stages: [
-    { duration: "1m", target: 100 }, // 10 users selama 1 menit
+    { duration: "2m", target: 1000 }, // 10 users selama 1 menit
   ],
 };
 
